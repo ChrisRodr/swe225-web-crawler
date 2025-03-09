ca09599d39a4cc18a1534d5d04d0750c586656aa
Christianrodriguez795@yahoo.com <44853624+ChrisRodr@users.noreply.github.com>
2025-03-08 20:40:26 -0800

    Added status prints

--- generate_document_norm.py
+++ generate_document_norm.py
@@ -3,19 +3,27 @@ from collections import defaultdict
 from utils.file_handler import inverted_index_postings
 
 def generate_doc_norm_file(output_dir):
+	print("Reading files and creating doc_squared_sum...")
 	# Compute squared sum of TF-IDF values for each document
+	num_postings_read = 0
 	doc_squared_sum = defaultdict(float)
 	for row in inverted_index_postings(output_dir):
+		num_postings_read += 1
 		doc_id = row[0]
 		doc_tfidf = float(row[1])
 		doc_squared_sum[doc_id] += doc_tfidf ** 2
+		print(f"\rNumber of postings processed: {num_postings_read}", end='')
+		sys.stdout.flush()
 
 	# Compute the L2 Norm for each document
+	print("\nCalculating the doc_norms")
 	doc_norms = {doc_id: math.sqrt(sq_sum) for doc_id, sq_sum in doc_squared_sum.items()}
+	print("Sorting the doc norms by doc_id")
 	sorted_doc_norms = dict(sorted(doc_norms))
 
 	# Write this to a file
 	doc_norm_file = "doc_norms.txt"
+	print(f"Writing the doc norms to {doc_norm_file} file")
 	with open(doc_norm_file, mode='w') as file:
 		for doc_id, norm in sorted_doc_norms.items():
 			file.write(f"{doc_id} {norm}\n")
